49a237937b59dbb4acacef8ea5bc1fa0ea18f28b
w-Adm <khost@bk.ru>
2023-04-06 15:01:20 +0300

    something wrong, but save. 06.04.2023

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
-from kivy_garden.graph import Graph
+from kivy_garden.graph import Graph, LinePlot
 import numpy as np
 
 Window.size = (400 , 800)
@@ -13,8 +13,12 @@ class MainApp(App):
 class Plotter(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.graph = Graph()
+        self.samples = 512
+        self.graph = Graph(xmin=0, xmax=512, ymin=0, ymax=512)
         self.ids.graph.add_widget(self.graph)
-
+        self.plot_x = np.linspace(0,1, self.samples)
+        self.plot_y = np.linspace(0,1, self.samples)
+        self.plot = LinePlot()
+        self.plot.points = [range(2)]
 
 MainApp().run()
