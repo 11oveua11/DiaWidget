ef4f12a28635bee77a0e5a3a28a5e38f9c495108
w-Adm <khost@bk.ru>
2023-04-06 14:31:04 +0300

    save 06.04.2023

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
+from kivy_garden.graph import Graph
+import numpy as np
 
-Window.size = (600 , 1000)
+Window.size = (400 , 800)
 
 class MainApp(App):
     def build(self):
         return Plotter()
 
 class Plotter(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.graph = Graph()
+        self.ids.graph.add_widget(self.graph)
 
 
 MainApp().run()
