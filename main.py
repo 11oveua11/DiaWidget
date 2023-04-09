1b000f4fbe53bde7b9a4ae58bf425b31ecc053af
Adm <khost@bk.ru>
2023-04-10 02:04:45 +0300

    Better Graph! Go ahead.. Now I need to implement my currency code.

--- main.py
+++ main.py
@@ -14,11 +14,15 @@ class Plotter(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.samples = 32
-        self.graph = Graph(xmin=0, xmax=self.samples, ymin=-1, ymax=1)
+        self.graph = Graph(xmin=0, xmax=self.samples, ymin=-1, ymax=1,
+                           border_color=[0,1,1,1], tick_color=[0,1,1,0.7],
+                           x_grid=True, y_grid=True, draw_border=True,
+                           x_grid_label=True, y_grid_label=False,
+                           x_ticks_major=4, y_ticks_major=0.4)
         self.ids.graph.add_widget(self.graph)
         self.plot_x = np.linspace(0, 1, self.samples)
         self.plot_y = np.sin(4*np.pi*self.plot_x)
-        self.plot = LinePlot()
+        self.plot = LinePlot(color=[1,1,0,1], line_width=1.5)
         self.plot.points = ([(x, self.plot_y[x]) for x in range(self.samples)])
         self.graph.add_plot(self.plot)
 
